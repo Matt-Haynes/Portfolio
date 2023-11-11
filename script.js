57e36acd64fba4748edd391bd1a2401c80d6f096
Matt Haynes <matt_developer@outlook.com>
2023-11-11 19:35:59 +0000

    update 15

--- script.js
+++ script.js
@@ -1,44 +1,23 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.addEventListener("DOMContentLoaded", function() {
+    // Update Year in Footer
+    document.getElementById('current-year').textContent = new Date().getFullYear();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
+    // Smooth Scrolling for Navbar Links
+    document.querySelectorAll('.navbar a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
         });
     });
+
+    // Modal Functionality
+    window.openModal = function(modalId) {
+        document.getElementById(modalId).style.display = 'block';
+    }
+
+    window.closeModal = function(modalId) {
+        document.getElementById(modalId).style.display = 'none';
+    }
 });
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the image and insert it inside the modal
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
-function openModal(modalId) {
-    var modal = document.getElementById(modalId);
-    var img = document.getElementById(modalId.replace('modal', 'img'));
-    var modalImg = modal.getElementsByClassName("modal-content")[0];
-    var captionText = modal.getElementById("caption");
-
-    modal.style.display = "block";
-    modalImg.src = img.src;
-    captionText.innerHTML = img.alt;
-}
-
-function closeModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "none";
-}
